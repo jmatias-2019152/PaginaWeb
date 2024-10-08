73be59053f6d774c56f2866ee43d079a0b9c9fb8
unknown <jmatias-2019152@kinal.edu.gt>
2024-10-08 10:48:01 -0600

    Bloqueo

--- js/bloquear.js
+++ js/bloquear.js
@@ -1,22 +1,4 @@
-// Función para deshabilitar el zoom
-function disableZoom() {
-    document.body.addEventListener('wheel', function (event) {
-        event.preventDefault();
-    }, { passive: false });
-
-    document.body.addEventListener('gesturestart', function (event) {
-        event.preventDefault();
-    });
-}
-
-// Establece el zoom inicial (alejado)
-function setInitialZoom() {
-    document.body.style.transform = 'scale(0.8)'; // Ajusta el valor según lo necesites
-    document.body.style.transformOrigin = '0 0';
-}
-
-// Llama a las funciones cuando la página se carga
-window.onload = function () {
-    disableZoom();
-    setInitialZoom();
-};
+// Deshabilitar el zoom con gestos de pellizco en dispositivos móviles
+document.addEventListener('gesturestart', function (event) {
+    event.preventDefault();
+});
