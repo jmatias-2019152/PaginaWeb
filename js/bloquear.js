314aef427aa9b114a3370e746adfbfe748f3d071
unknown <jmatias-2019152@kinal.edu.gt>
2024-10-08 10:50:21 -0600

    Bloqueo x

--- js/bloquear.js
+++ js/bloquear.js
@@ -2,3 +2,10 @@
 document.addEventListener('gesturestart', function (event) {
     event.preventDefault();
 });
+
+// Evitar el zoom en el evento de rueda del mouse
+window.addEventListener('wheel', function(event) {
+    if (event.ctrlKey) {
+        event.preventDefault();
+    }
+}, { passive: false });
